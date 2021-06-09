6522c7c3e30c33f87fcb053b33cba091b4b1786a
nagarjungautham <nagarjungautham@gmail.com>
2021-06-10 03:54:13 +0530

    Previous commit: Throw exception for negative numbers Ignore value greater than 1000 Current commit: Refactored code

--- src/main/java/com/tdd/calculator/CalculatorValidator.java
+++ src/main/java/com/tdd/calculator/CalculatorValidator.java
@@ -9,14 +9,11 @@ public class CalculatorValidator implements ToIntFunction<String> {
 
     @Override
     public int applyAsInt(String numbers) {
-        if(numbers.isEmpty()) {
-            return 0;
-        }
-        else if(numbers.contains(",")){
+        if(numbers.contains(",")){
             String[] arr = numbers.split("[/!\"#$%&'*+,.:;=?@^_`|-~\n]");
             int[] intArray = Arrays.stream(arr).mapToInt(num -> (num.isEmpty()||parseInt(num)>1000) ? 0 : parseInt(num)).toArray();
 
-            //To get Negative Numbers
+            //To get Negative Numbers and Throw exception
             StringBuilder negatives = new StringBuilder();
             for (int a:intArray) if(a<0) negatives.append(a).append(" ");
             if(negatives.length() > 0) throw new RuntimeException(String.valueOf(negatives));
@@ -25,7 +22,9 @@ public class CalculatorValidator implements ToIntFunction<String> {
             return Arrays.stream(intArray).sum();
         }
         else {
-            return parseInt(numbers);
+            //For empty string and single value
+            ToIntFunction<String> defaultValue = a -> a.isEmpty() ? 0 : parseInt(a);
+            return defaultValue.applyAsInt(numbers);
         }
     }
 }
